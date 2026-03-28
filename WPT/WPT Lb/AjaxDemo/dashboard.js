330090984af910f92677f8bb2c2d3d45dab844f2
unknown <sagarabkr19@gmail.com>
2026-03-28 18:20:36 +0530

    ajaxTask2

--- WPT/WPT Lb/AjaxDemo/dashboard.js
+++ WPT/WPT Lb/AjaxDemo/dashboard.js
@@ -1,127 +1,32 @@
-// function getData() {
-//   let xmlhttp = new XMLHttpRequest();
-//   xmlhttp.onload = function () {
-//     // console.log(this.responseText);
-//     let data = JSON.parse(this.responseText);
-//     console.log(data);
-//     let tbody = document.getElementById("tb");
-//     for (let i = 0; i < data.length; i++) {
-//       let newRow = document.createElement("tr");
-//       newRow.innerHTML = `<td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].role}</td><td>${data[i].salary}</td><td>${data[i].department}</td>`;
-//       tbody.appendChild(newRow);
-//     }
-//   };
-//   xmlhttp.open("GET", "http://localhost:8888/employees"); //three parameter(req para, url, true(asynchronous)/false(not work in asynchronous))
-//   xmlhttp.send();
-
-//   // window.alert("*** (..)Hi this is Sagar(..) ***");
-// }
-
-// function deleteRecord()
-// {
-//   if(window.confirm(`Are you sure to delete record with id: ${id}`))
-//   {
-//     let id = document.getElementById("uid").value;
-//     let xmlhttp = new XMLHttpRequest()
-//     xmlhttp.onload = function()
-//     {
-//       window.alert("Record deleted succesfully");
-//     };
-//     xmlhttp.open("DELETE",`http://localhost:8888/employees/${id}`);
-//     xmlhttp.send();
-//   }
-// }
-
-// GET DATA
-function getData() {
-  let xmlhttp = new XMLHttpRequest();
-
-  xmlhttp.onload = function () {
-    let data = JSON.parse(this.responseText);
-
-    let tbody = document.getElementById("tb");
-    tbody.innerHTML = "";   // FIX: clear old data
-
-    for (let i = 0; i < data.length; i++) {
-      let row = document.createElement("tr");
-
-      row.innerHTML =
-        `<td>${data[i].id}</td>
-         <td>${data[i].name}</td>
-         <td>${data[i].role}</td>
-         <td>${data[i].salary}</td>
-         <td>${data[i].department}</td>`;
-
-      tbody.appendChild(row);
+function getData()
+{
+    let xmlhttp =new XMLHttpRequest();
+    xmlhttp.onload= function()
+    {
+        let data = JSON.parse(this.responseText)
+        let parentE1=document.getElementById("d1")
+        for(let i = 0; i<data.length; i++)
+        {
+            let newTR = document.createElement("tr");
+            newTR.innerHTML = `<td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].age}</td><td>${data[i].email}</td><td>${data[i].city}</td>`;
+            parentE1.appendChild(newTR)
+        }
     }
-  };
-
-  xmlhttp.open("GET", "http://localhost:8888/employees");
-  xmlhttp.send();
-}
-
-// DELETE
-function deleteRecord() {
-  let id = document.getElementById("uid").value;   // FIX: define first
-
-  if (window.confirm("Delete record with ID: " + id)) {
-    let xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onload = function () {
-      alert("Deleted successfully");
-      getData();
-    };
-
-    xmlhttp.open("DELETE", `http://localhost:8888/employees/${id}`);
+    xmlhttp.open("GET",`http://localhost:8888/users`);
     xmlhttp.send();
-  }
 }
 
-// ADD (POST)
-function addRecord() {
-  let data = {
-    name: document.getElementById("name").value,
-    role: document.getElementById("role").value,
-    salary: document.getElementById("salary").value,
-    department: document.getElementById("dept").value
-  };
-
-  let xmlhttp = new XMLHttpRequest();
-
-  xmlhttp.onload = function () {
-    alert("Record added");
-    document.getElementById("name").value=""
-    document.getElementById("role").value=""
-    document.getElementById("salary").value=""
-    document.getElementById("dept").value=""
-    getData();
-  };
-
-  xmlhttp.open("POST", "http://localhost:8888/employees");
-  xmlhttp.setRequestHeader("Content-Type", "application/json");
-  xmlhttp.send(JSON.stringify(data));
-}
-
-// UPDATE (PUT)
-function updateRecord() {
-  let id = document.getElementById("uid_update").value;
-
-  let data = {
-    id: Number(id),
-    name: document.getElementById("name_update").value,
-    role: document.getElementById("role_update").value,
-    salary: document.getElementById("salary_update").value,
-    department: document.getElementById("dept_update").value
-  };
-
-  let xmlhttp = new XMLHttpRequest();
-
-  xmlhttp.onload = function () {
-    alert("Record updated");
-    getData();
-  };
-
-  xmlhttp.open("PUT", `http://localhost:8888/employees/${id}`);
-  xmlhttp.setRequestHeader("Content-Type", "application/json");
-  xmlhttp.send(JSON.stringify(data));
+function deleteRecord()
+{
+    let id = document.getElementById("uid").value;
+    if(window.confirm(`Are you sure you want to delete this id:${id}`))
+    {
+        let xmlhttps =new XMLHttpRequest()
+        xmlhttps.onload = function()
+        {
+            alert("Record deleted Successfully")
+        };
+        xmlhttps.open("DELETE", `http://localhost:8888/users/${id}`)
+        xmlhttps.send();
+    }
 }
