a6446ebbed2b4766f154822605677a0152f6b42c
unknown <sagarabkr19@gmail.com>
2026-04-17 15:40:09 +0530

    java

--- JavaLab/day_10/src/treePractice/Mycomparator.java
+++ JavaLab/day_10/src/treePractice/Mycomparator.java
@@ -6,26 +6,38 @@ public class Mycomparator implements Comparator<Employee> {
 
 	@Override
 	public int compare(Employee e1, Employee e2) {
-		if(e1.getName().charAt(0) > e2.getName().charAt(0))
-		{
-			return +1;
-		}
-		else if(e1.getName().charAt(0) < e2.getName().charAt(0))
-		{
-			return -1;
-		}
-		else
-		{
-			for(int i = 0; i < Math.max(e1.getName().length(), e1.getName().length()); i++)
+		String cm1 = e1.getName().toLowerCase(); 
+		String cm2 = e2.getName().toLowerCase();
+		if (cm1.charAt(0) > cm2.charAt(0)) 
 			{
-				if(e1.getName().charAt(i) < e2.getName().charAt(i))
-				{
-					return -1;
+				return +1;
+			} else if (cm1.charAt(0) < cm2.charAt(0)) 
+			{
+				return -1;
+			} else {
+				for (int i = 0; i < Math.max(cm1.length(), cm1.length()); i++) {
+					if (cm1.charAt(i) > cm2.charAt(i)) {
+						return +1;
+					}
 				}
+				return 0;
 			}
-			return 0;
-		}
 		
-	}
+		//Descending order
+//		if (cm1.charAt(0) < cm2.charAt(0)) 
+//		{
+//			return +1;
+//		} else if (cm1.charAt(0) > cm2.charAt(0)) 
+//		{
+//			return -1;
+//		} else {
+//			for (int i = 0; i < Math.max(cm1.length(), cm1.length()); i++) {
+//				if (cm1.charAt(i) < cm2.charAt(i)) {
+//					return +1;
+//				}
+//			}
+//			return 0;
+//		}
 
+	}
 }
