26891cea43d3a52ea58901b76b2400c8429d7b47
unknown <sagarabkr19@gmail.com>
2026-05-02 00:45:54 +0530

    BinaryTree

--- ADS_workSpace/classWork/src/dsa/binaryTree/BinaryTreeDemo.java
+++ ADS_workSpace/classWork/src/dsa/binaryTree/BinaryTreeDemo.java
@@ -4,8 +4,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class BTNode {
-	private int data;
-	private BTNode left, right;
+	protected int data;
+	protected BTNode left, right;
 
 	public BTNode() {
 		this.data = 0;
@@ -291,8 +291,7 @@ class MyBinaryTree {
 		}
 		return min;
 	}
-	
-	
+
 	public void displayLevelWiseMaxMin() {
 		if (root == null) {
 			System.out.println("Empty.....");
@@ -307,25 +306,66 @@ class MyBinaryTree {
 		System.out.print("Levelwise: ");
 		while (!q.isEmpty()) {
 			temp = q.poll();
-			if(max < temp.getData())
-			{
+			if (max < temp.getData()) {
 				max = temp.getData();
 			}
-			if(min > temp.getData())
-			{
+			if (min > temp.getData()) {
 				min = temp.getData();
 			}
-			if (temp.getLeft() != null) 
-			{
+			if (temp.getLeft() != null) {
 				q.offer(temp.getLeft());
-			} 
+			}
 			if (temp.getRight() != null) {
 				q.offer(temp.getRight());
 			}
 		}
-		
-		System.out.println("Min: "+min);
-		System.out.println("Max: "+max);
+
+		System.out.println("Min: " + min);
+		System.out.println("Max: " + max);
+	}
+
+	public boolean treeCompare(MyBinaryTree tree) {
+		BTNode iter1, iter2;
+		if (root == null || tree.root == null) {
+			return false;
+		}
+		Queue<BTNode> q1 = new LinkedList<BTNode>();
+		Queue<BTNode> q2 = new LinkedList<BTNode>();
+
+		iter1 = root;
+		iter2 = tree.root;
+
+		q1.add(iter1);
+		q2.add(iter2);
+
+		while ((!q1.isEmpty()) && (!q2.isEmpty())) {
+			iter1 = q1.remove();
+			iter2 = q2.remove();
+
+			if (iter1.data != iter2.data) {
+				return false;
+			}
+
+			if (iter1.left != null) {
+				q1.add(iter1.left);
+			}
+			if (iter1.right != null) {
+				q1.add(iter1.right);
+			}
+
+			if (iter2.left != null) {
+				q2.add(iter2.left);
+			}
+			if (iter2.right != null) {
+				q2.add(iter2.right);
+			}
+
+		}
+		if (q1.isEmpty() && q2.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
@@ -350,6 +390,24 @@ public class BinaryTreeDemo {
 		bt.getRoot().getLeft().getRight().setLeft(bt.createNode(19));
 		bt.getRoot().getRight().getLeft().setRight(bt.createNode(67));
 
+		MyBinaryTree bt2 = new MyBinaryTree();
+		bt2.setRoot(bt2.createNode(50));
+
+		bt2.getRoot().setLeft(bt2.createNode(17));
+		bt2.getRoot().setRight(bt2.createNode(72));
+
+		bt2.getRoot().getLeft().setLeft(bt2.createNode(12));
+		bt2.getRoot().getLeft().setRight(bt2.createNode(23));
+
+		bt2.getRoot().getRight().setLeft(bt2.createNode(54));
+		bt2.getRoot().getRight().setRight(bt2.createNode(76));
+
+		bt2.getRoot().getLeft().getLeft().setLeft(bt2.createNode(9));
+		bt2.getRoot().getLeft().getLeft().setRight(bt2.createNode(14));
+
+		bt2.getRoot().getLeft().getRight().setLeft(bt2.createNode(19));
+		bt2.getRoot().getRight().getLeft().setRight(bt2.createNode(67));
+
 //		bt.PreOrder();
 //		System.out.println();
 //		bt.InOrder();
@@ -369,5 +427,6 @@ public class BinaryTreeDemo {
 		System.out.println();
 		bt.displayLevelWiseMaxMin();
 
+		System.out.println("Compare: " + bt.treeCompare(bt2));
 	}
 }
