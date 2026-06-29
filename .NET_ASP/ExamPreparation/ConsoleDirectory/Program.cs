1f66e4ffedb8c99e06cb07584a22816b1f8204b8
Saisagar Ambekar <sagarabkr19@gmail.com>
2026-06-29 20:28:32 +0530

    ExamPrepDirectory

---
+++ .NET_ASP/ExamPreparation/ConsoleDirectory/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Directory Data ");
+        
+            try
+            {
+                foreach (var drive in DriveInfo.GetDrives())
+                {
+                    if (drive.DriveType != DriveType.Network)
+                    {
+                        Console.WriteLine($"Drive Name : {drive.Name}");
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Console.WriteLine(uae.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+            //For specific directory
+            string dirName = @"C:\";
+            DirectoryInfo directoryInfo = new DirectoryInfo(dirName);
+            Console.WriteLine($"Currently in DriveName:  {directoryInfo.Root}");
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+
+                Console.WriteLine($"\t\t FolderName: {directory.Name}");
+                try
+                {
+                    foreach (var files in directory.GetFiles())
+                    {
+                        Console.WriteLine($"\t\t\t\tFile Name: {files.Name}");
+                    }
+                }
+                catch (UnauthorizedAccessException uae)
+                {
+                    Console.WriteLine(uae.Message);
+                }
+            }
+        }
+    }
+}
